ccea7b8e65449c3c0fd5da0de1f9503ae72a7030
https-404 <areesh.192003@gmail.com>
2024-07-12 04:38:54 +0500

    Enhanced security and Swagger config
    - In WeatherForecastController.cs, added necessary `using` directives for ASP.NET Core's Authorization and MVC features. Also, updated the `HttpGet` method with the `Authorize` attribute to enforce user authorization for accessing weather forecast data.
    
    - In Program.cs, expanded the Swagger/OpenAPI setup with additional `using` directives for enhanced functionality and configuration, including security definitions for OAuth2 via an API key. Also, integrated ASP.NET Core Authorization with the addition of `AddAuthorization()` method to support the `[Authorize]` attribute in controllers.
    
    - Updated dotnetAuthentications.csproj by adding a reference to `Swashbuckle.AspNetCore.Filters` version `8.0.2` to enrich Swagger/OpenAPI documentation with additional features like request examples, response examples, and filters.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using dotnetAuthentications.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +14,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(
+ options => {
+     options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+{
+    In = ParameterLocation.Header,
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey
+}
+    );
+     options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+});
+
 builder.Services.AddDbContext<DataContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
