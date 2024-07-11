ea3f786f85c6ad9a47e9d85e7f4a31500b01a004
https-404 <areesh.192003@gmail.com>
2024-07-12 04:27:01 +0500

    Integrate EF Core, Identity, and Swagger
    Added Entity Framework Core and ASP.NET Core Identity dependencies to the project for data access and authentication management. Configured the DataContext class for the database context and updated the appsettings.json with the SQL Server connection string. Enhanced the Program.cs to register database and identity services, set up authorization, and map Identity API endpoints. Integrated Swagger for API documentation and testing.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using dotnetAuthentications.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+builder.Services.AddAuthorization();
 
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<DataContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapIdentityApi<IdentityUser>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
